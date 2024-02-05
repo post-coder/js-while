f863ae52f9b81d6d6ea0024c7a5cca6d274a8355
post-coder <gabrielspanu.dev@gmail.com>
2024-02-05 12:13:45 +0100

    do while

--- js/script.js
+++ js/script.js
@@ -6,71 +6,26 @@ sia con il ciclo for che con il ciclo while
  */
 
 
-const array = [
-    'Alice', 'Bob', 'Charlie', 'David', 'Emily',
-    'Frank', 'Grace', 'Henry', 'Ivy', 'Jack',
-    'Kate', 'Liam', 'Mia', 'Noah', 'Olivia',
-    'Patrick', 'Quinn', 'Rachel', 'Samuel', 'Tracy',
-    'Ursula', 'Victor', 'Wendy', 'Xander', 'Yvonne',
-    'Zane', 'Ava', 'Benjamin', 'Chloe', 'Daniel',
-    'Ella', 'Finn', 'Gabriella', 'Harrison', 'Isabella',
-    'Jackson', 'Kylie', 'Landon', 'Mila', 'Nathan',
-    'Oliver', 'Penelope', 'Quentin', 'Rebecca', 'Sophia',
-    'Tyler', 'Uma', 'Vincent', 'Willow', 'Xavier',
-    'Yasmine', 'Zachary', 'Abigail', 'Bryan', 'Catherine',
-    'Dylan', 'Eva', 'Freddie', 'Georgia', 'Hayden',
-    'Isaac', 'Jasmine', 'Kevin', 'Lila', 'Mason',
-    'Natalie', 'Oscar', 'Peyton', 'Quincy', 'Riley',
-    'Samantha', 'Toby', 'Ulysses', 'Violet', 'Walter',
-    'Xena', 'Yara', 'Zara', 'Adam', 'Bella',
-    'Caleb', 'Diana', 'Ethan', 'Fiona', 'George',
-    'Hannah', 'Ian', 'Jade', 'Kyle', 'Luna'
-];
+// inserisci un numero
+let number;
 
-// let trovato = false;
+// il ciclo do-while PRIMA esegue le operazioni dentro le graffe
+// solo DOPO controlla se la condizione è vera, in quel caso continua a ripeterle
+// con questo tipo di ciclo abbiamo la sicurezza che le 
+// istruzioni dentro le graffe vengano eseguite ALMENO UNA VOLTA.
+do {
 
-// // stampiamolo con il ciclo for
-// for(let i = 0; i < array.length; i++) {
+    number = prompt("Inserisci un numero");
 
-//     if(array[i] == "Alice") {
-//         trovato = true;
-//     }
-
-//     console.log("operazioni eseguite");
-
-// }
-
-
-
-// _________________________________
-
-
-
-let trovato = false;
-
-// inizializziamo il contatore
-let contatore = 0;
-
-// sinchè il nome non è stato trovato
-while(trovato == false && contatore < array.length) {
-
-    // controlliamo che il nome della lista sia uguale a quello che cerchiamo
-    if(array[contatore] == "Frank") {
-        trovato = true;
+    if(isNaN(number)) {
+        alert("Il numero inserito non è valido");
     }
 
-    // modificare la condizione di ciclo
-    contatore++;
-
-    console.log("operazioni eseguite");
-
-}
+} while (isNaN(number));
 
 
 
+const numberElevated = number ** 3;
 
-if(trovato) {
-    console.log("Trovato!");
-} else {
-    console.log("Alunno non presente");
-}
+console.log(numberElevated);
+
